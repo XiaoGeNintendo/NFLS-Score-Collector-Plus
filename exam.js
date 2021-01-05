48c76fbfc75c56f6292229ffd0c92daec496d43f
XiaoGeNintendo <34835642+XiaoGeNintendo@users.noreply.github.com>
2021-01-05 20:07:54 +0800

    Create exam.js

---
+++ exam.js
@@ -0,0 +1,19 @@
+var __version="TEMPLATE"
+
+var __exams=[
+    {
+        name:"NFLS Monthly Test of Junior Three Term 1 #1", //name
+        icon:"book", //in font awesome code
+        subject:[{icon:"book",name:"Chinese",max:120},{icon:"cubes",name:"Maths",max:120},{icon:"language",name:"English",max:100},{icon:"magnet",name:"Physics",max:100},{icon:"eyedropper",name:"Chemistry",max:80},{icon:"institution",name:"History",max:60},{icon:"flag",name:"Politics",max:60}],sum:[{name:"CME",sum:[0,1,2]},{name:"PC",sum:[3,4]},{name:"HP",sum:[5,6]},{name:"TOT",sum:[0,1,2,3,4,5,6]}],
+        user:["Ninetail","Hikari","Doragon","Ookami","Rinri","Rinko","Lo","Ginrin"], //users
+        data:[
+            [100,88,97,102,98,96,90,92],
+            [101,107,103,115,110,97,104,96],
+            [87,84.5,90,89.5,88,85,85,81],
+            [98,91,93,99,91,88,87,85],
+            [71,73,77,73,72,70,62,71],
+            [53,52,56,52,52,47,39,51],
+            [39,36,48,45,41,34,34,33]
+        ] //TODO
+    },
+]
